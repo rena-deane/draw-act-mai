070bcd3ed9bb0cbae87b3e81f1f6d34f100cb6ca
Rena Goldsmith <rena.kg@gmail.com>
2016-05-29 00:44:44 +1200

    show stack entry and bottom row after countdown

--- js/builder.js
+++ js/builder.js
@@ -4,12 +4,19 @@ getStack.addEventListener("click", function (){
   document.getElementById("myDropdown").classList.toggle("show");
 })
 
-var liveScore = 1;
+// add score
 var addCorrect = document.getElementById('correct');
 addCorrect.addEventListener("click", function(){
-  $("#score").html(liveScore);
+  addScore();
+});
+
+
+var liveScore = 0;
+function addScore(){
   liveScore += 1;
-})
+  $('#score').html(liveScore);
+}
+
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
@@ -27,12 +34,11 @@ window.onclick = function(event) {
 
 // 3 2 1 Countdown
 var timer;
-function  countdown(secs, element){
-
+function  countdown(secs, element, func){
   var showSec = document.getElementById(element);
-  
   if(secs === 3){
     showSec.innerHTML = "On Your Marks";
+    $('.pass-correct').addClass('show');
   }
   if(secs === 2){
     showSec.innerHTML = "Get Set";
@@ -42,24 +48,23 @@ function  countdown(secs, element){
   }
   secs--;
   console.log(secs);
-  timer = setTimeout('countdown('+secs+',"'+element+'")', 1200);
+  timer = setTimeout('countdown('+secs+',"'+element+'", '+func+')', 1200);
   
-  if (secs < 1){
+  if (secs < 0){
     clearTimeout(timer);
-    showMythsLegends();
+    func();
   }
 }
 
-var startGame = document.getElementById('start');
-startGame.addEventListener("click", function(){
-  countdown(3, "entry")
-});
+var playCat1 = document.getElementById('cat1');
+playCat1.addEventListener("click", function(){
+  countdown(3, "entry", showMythsLegends);
+})
 
 // Choose Stack and Play
 function showMythsLegends (){
   var generateIndex = Math.floor(maoriMyth.length * Math.random());
 
   var grabMythsLegends = maoriMyth[generateIndex];
-  $("#entry").html(grabMythsLegends);
-
+  $('#entry').html(grabMythsLegends);
 }
